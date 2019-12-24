0831614d66c350de695b53148f3a438600afbd2b
RyBra <kitaize@yandex.ru>
2019-12-24 14:50:59 +0300

    Add check app location

--- src/App.js
+++ src/App.js
@@ -2,55 +2,99 @@ import React, { PureComponent as Component } from "react";
 import ChatWindow from "./components/ChatWindow";
 import Launcher from "./components/Launcher";
 
+const URL =
+  window.location.host !== "localhost:3000"
+    ? `wss://${window.location.host}/ws/vitacat/chat/`
+    : `ws://localhost:8000/ws/vitacat/chat/`;
+
 class App extends Component {
-  state = {
-    chatOpen: false,
-    new_message: {
-      recived: false,
-      message: ""
-    },
-    messages: [
-      {
-        recived: true,
-        message: "Привет"
-      },
-      {
+  ws = new WebSocket(URL);
+
+  constructor() {
+    super();
+    this.state = {
+      chatOpen: false,
+      new_message: {
         recived: false,
-        message: "Привет"
+        message: ""
       },
-      {
-        recived: false,
-        message: "Как дела"
-      },
-      {
-        recived: true,
-        message: "Нормально"
-      },
-      {
-        recived: false,
-        message: "Хорошо"
-      },
-      {
-        recived: true,
-        message: "Я сломался"
-      }
-    ]
-  };
+      messages: []
+    };
+  }
+
+  componentDidMount() {
+    this.ws.onopen = () => {};
+
+    this.ws.onmessage = evt => {
+      const message = JSON.parse(evt.data);
+      this.setState({
+        messages: Array.isArray(message)
+          ? [...this.state.messages, ...message]
+          : [...this.state.messages, message]
+      });
+    };
+
+    this.ws.onclose = () => {
+      // Автоматическое подключения если соккет упал
+      this.setState({
+        ws: new WebSocket(URL)
+      });
+    };
+  }
+
+  // state = {
+  //   chatOpen: false,
+  //   new_message: {
+  //     recived: false,
+  //     message: ""
+  //   },
+  //   messages: [
+  //     {
+  //       recived: true,
+  //       message: "Привет"
+  //     },
+  //     {
+  //       recived: false,
+  //       message: "Привет"
+  //     },
+  //     {
+  //       recived: false,
+  //       message: "Как дела"
+  //     },
+  //     {
+  //       recived: true,
+  //       message: "Нормально"
+  //     },
+  //     {
+  //       recived: false,
+  //       message: "Хорошо"
+  //     },
+  //     {
+  //       recived: true,
+  //       message: "Я сломался"
+  //     }
+  //   ]
+  // };
 
   onClickOpenChat = e => {
     this.setState({ chatOpen: !this.state.chatOpen });
   };
 
+  sendMessage = message => {
+    this.ws.send(JSON.stringify(message));
+    this.setState({
+      messages: [...this.state.messages, message],
+      new_message: {
+        recived: false,
+        message: ""
+      }
+    });
+  };
+
   _handleSendMessage = e => {
     if (this.state.new_message.message) {
-      const newMessages = [...this.state.messages, this.state.new_message];
-      this.setState({
-        new_message: {
-          recived: false,
-          message: ""
-        },
-        messages: newMessages
-      });
+      let message = this.state.new_message;
+      this.sendMessage(message);
     }
   };
 
@@ -66,14 +110,8 @@ class App extends Component {
   _handleKeyDown = e => {
     if (e.keyCode === 13 && !e.shiftKey) {
       if (this.state.new_message.message) {
-        const newMessages = [...this.state.messages, this.state.new_message];
-        this.setState({
-          new_message: {
-            recived: false,
-            message: ""
-          },
-          messages: newMessages
-        });
+        let message = this.state.new_message;
+        this.sendMessage(message);
       }
     }
   };
